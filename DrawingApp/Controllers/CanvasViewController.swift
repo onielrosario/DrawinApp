f524f319277b18096989177c5ae8f7c66cc669bf
onielrosario <onielrosario@pursuit.org>
2020-02-19 19:28:41 -0700

    pushing changes new background color change feature

--- DrawingApp/Controllers/CanvasViewController.swift
+++ DrawingApp/Controllers/CanvasViewController.swift
@@ -14,8 +14,11 @@ class CanvasViewController: UIViewController {
     @IBOutlet weak var strokeWidthSlider: UISlider!
     @IBOutlet weak var canvasTrailingC: NSLayoutConstraint!
     @IBOutlet weak var canvasLeadingC: NSLayoutConstraint!
+    @IBOutlet weak var changeBackgroundView: UIView!
+    @IBOutlet weak var backgroundColorSelection: UIView!
+    private var backgroundFill: UIColor?
+    private var slideBackgroundColor = false
     private var slideMenu = false
-    
     lazy var canvas = Canvas()
     
     override func viewDidLoad() {
@@ -25,6 +28,7 @@ class CanvasViewController: UIViewController {
     }
     
     fileprivate func setupUI() {
+        changeBackgroundView.center.x += 300
         strokeWidthSlider.thumbTintColor = .black
         strokeWidthSlider.tintColor = .black
         canvas.backgroundColor = .white
@@ -38,13 +42,27 @@ class CanvasViewController: UIViewController {
     }
     
     @IBAction func clear(_ sender: UIButton) {
-        showAlert(title: nil, message: "So you want to erase all your progress?", style: .alert) { (Ok) in
+        showAlert(title: nil, message: "Do you want to erase all your  current progress?", style: .alert) { (Ok) in
             self.canvas.clear()
             self.strokeWidthSlider.thumbTintColor = .black
             self.strokeWidthSlider.tintColor = .black
+            self.canvas.backgroundColor = .white
             self.strokeWidthSlider.setValue(0.5, animated: true)
         }
     }
+    
+    @IBAction func changeBackgrounButton(_ sender: UIButton) {
+         popBackgroundColorChange()
+    }
+    
+    
+    @IBAction func selectBackgroundColor(_ sender: UIButton) {
+        if slideBackgroundColor {
+            canvas.backgroundColor = backgroundColorSelection.backgroundColor
+            popBackgroundColorChange()
+        }
+    }
+    
     
     @IBAction func setStrokeWidth(_ sender: UISlider) {
         canvas.setStrokeWidth(value: CGFloat(sender.value))
@@ -56,24 +74,18 @@ class CanvasViewController: UIViewController {
     
     @IBAction func saveAss(_ sender: UIButton) {
         let image = canvas.getImageFromCanvas()
-        showAlert(title: nil, message: "Do you want to save your photo?", style: .alert) { _ in
+        showAlert(title: nil, message: "Do you want to save your photo?", style: .alert) { (ok) in
             UIImageWriteToSavedPhotosAlbum(image, self, #selector(self.image(_:didFinishSavingWithError:contextInfo:)), nil)
+            self.popMenu()
         }
-        popMenu()
     }
     
     @objc fileprivate func image(_ image: UIImage, didFinishSavingWithError error: Error?, contextInfo: UnsafeRawPointer) {
         if let error = error {
-            // we got back an error!
-            let ac = UIAlertController(title: "Save error", message: error.localizedDescription, preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
+            showAlert(title: "Error", message: error.localizedDescription, actionTitle: "OK")
         } else {
-            let ac = UIAlertController(title: "Saved!", message: nil, preferredStyle: .alert)
-            ac.addAction(UIAlertAction(title: "OK", style: .default))
-            present(ac, animated: true)
+            showAlert(title: "Saved!", message: nil, actionTitle: "OK")
         }
-        
     }
     
     @IBAction func close(_ sender: UIButton) {
@@ -83,6 +95,19 @@ class CanvasViewController: UIViewController {
     @IBAction func taphandler(_ sender: UITapGestureRecognizer) {
         if slideMenu {
             popMenu()
+        }
+    }
+    
+    fileprivate func popBackgroundColorChange() {
+        slideBackgroundColor.toggle()
+        if slideBackgroundColor {
+            UIView.animate(withDuration: 0.5) {
+                self.changeBackgroundView.center.x -= 350
+            }
+        } else {
+            UIView.animate(withDuration: 0.5) {
+                           self.changeBackgroundView.center.x += 350
+                       }
         }
     }
     
@@ -113,8 +138,13 @@ class CanvasViewController: UIViewController {
 
 extension CanvasViewController: HSBColorPickerDelegate {
     func HSBColorColorPickerTouched(sender: HSBColorPicker, color: UIColor, point: CGPoint, state: UIGestureRecognizer.State) {
-        strokeWidthSlider.thumbTintColor = color
-        strokeWidthSlider.tintColor = color
-        canvas.strokeColor = color
+        if slideBackgroundColor {
+            backgroundFill = color
+            backgroundColorSelection.backgroundColor = color
+        } else {
+            strokeWidthSlider.thumbTintColor = color
+            strokeWidthSlider.tintColor = color
+            canvas.strokeColor = color
+        }
     }
 }
